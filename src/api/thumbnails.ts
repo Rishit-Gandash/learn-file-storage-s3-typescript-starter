1fc172550e61a44ad91e455265b59a4e940f353d
splitzerr <rishikgandash@gmail.com>
2026-07-02 15:28:18 +0530

    new: added thumbnail upload

--- src/api/thumbnails.ts
+++ src/api/thumbnails.ts
@@ -1,9 +1,9 @@
 import { getBearerToken, validateJWT } from "../auth";
 import { respondWithJSON } from "./json";
-import { getVideo } from "../db/videos";
+import { getVideo, updateVideo } from "../db/videos";
 import type { ApiConfig } from "../config";
 import type { BunRequest } from "bun";
-import { BadRequestError, NotFoundError } from "./errors";
+import { BadRequestError, NotFoundError, UserForbiddenError } from "./errors";
 
 type Thumbnail = {
   data: ArrayBuffer;
@@ -47,7 +47,35 @@ export async function handlerUploadThumbnail(cfg: ApiConfig, req: BunRequest) {
 
   console.log("uploading thumbnail for video", videoId, "by user", userID);
 
-  // TODO: implement the upload here
+  const formData = await req.formData();
+  const imageData = formData.get("thumbnail")
+  if(!(imageData instanceof File)) {
+      throw new BadRequestError("Not a File");
+  }
 
-  return respondWithJSON(200, null);
+  const MAX_UPLOAD_SIZE = 10 << 20;
+  if(imageData.size > MAX_UPLOAD_SIZE) {
+      throw new BadRequestError("Max upload size exceeded");
+  }
+  const fileType: string = imageData.type;
+  const fileData: ArrayBuffer = await imageData.arrayBuffer();
+
+  const videoMetaData = getVideo(cfg.db, videoId);
+
+  if(videoMetaData?.userID != userID) {
+      throw new UserForbiddenError("Invalid User");
+  }
+
+  videoThumbnails.set(videoId, {
+      data: fileData,
+      mediaType: fileType,
+  })
+
+  const thumbnailUrl = `http://localhost:<${cfg.port}>/api/thumbnails/<${videoId}>`;
+  videoMetaData.thumbnailURL = thumbnailUrl;
+  updateVideo(cfg.db, videoMetaData);
+
+
+
+  return respondWithJSON(200, videoMetaData);
 }
